0ce4600e5aadbaf8c239b2224184905febedcee1
rhallacker01 <48366396+rhallacker01@users.noreply.github.com>
2020-12-04 09:23:58 -0500

    Implement ratings-data-service
    Create a hardcoded API in ratings-data-service to return a rating given a movie ID: /ratingsdata/{movieId};
    	Create the Rest Controller Class
    	Create the Rating class
    	updated the property file to change from default port

---
+++ SpringbootMicroservice1/ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/models/Rating.java
@@ -0,0 +1,36 @@
+package io.javabrains.ratingsdataservice.models;
+
+public class Rating {
+
+//Class Variables
+	private String movieId;
+	private int rating;
+
+//Constructors
+	public Rating(String movieId, int rating) {
+		super();
+		this.movieId = movieId;
+		this.rating = rating;
+	}
+
+
+//Getters and Setters
+	
+	public String getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	
+}
