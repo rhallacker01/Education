a03ce42428a27b853d0bddfc6c7f27231be1f5ab
rhallacker01 <48366396+rhallacker01@users.noreply.github.com>
2020-12-11 09:20:55 -0500

    Microservices course
    Updated Microservices as Eureka Clients
    Added new maven dependencies for eureka
    Updated service names in property file
    Removed Hardcoded urls and Implemented Service discovery

--- SpringbootMicroservice1/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ SpringbootMicroservice1/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -32,11 +32,11 @@ public class MovieCatalogResource {
 		
 
 		
-	UserRating ratings = restTemplate.getForObject("http//localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+	UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
 		
 		return ratings.getUserRating().stream().map(rating -> {
 			//For each movie ID, call movie infor service and get details
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 
 			//Put them all together
 			return new CatalogItem(movie.getName(), "Desc", rating.getRating());
