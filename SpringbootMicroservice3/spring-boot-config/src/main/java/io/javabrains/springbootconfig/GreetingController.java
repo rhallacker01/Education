ddc70e32e3a4ef1da12c00b89227d20eddcd2704
rhallacker01 <48366396+rhallacker01@users.noreply.github.com>
2021-01-04 08:06:36 -0500

    Springboot Microservice 3
    @Value Annotation
    Use : to add in a default value so that program does not crash if there is no config specified
    	@Value("${my.greeting: default value}")
    
    Use , to create a list in the property file
    	list can be added to a variable without parsing and splitting manually
    
    Use # to manage maps of values

--- SpringbootMicroservice3/spring-boot-config/src/main/java/io/javabrains/springbootconfig/GreetingController.java
+++ SpringbootMicroservice3/spring-boot-config/src/main/java/io/javabrains/springbootconfig/GreetingController.java
@@ -1,5 +1,8 @@
 package io.javabrains.springbootconfig;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	@Value("${my.greeting}")
+	@Value("${my.greeting:default value}")
 	private String greetingMessage;
+	
+	@Value("some static message")
+	private String staticMessage;
+	
+	@Value("${my.list.values}")
+	private List<String> listValues;
+	
+	@Value("#{${dbValues}}")
+	private Map<String, String> dbValues;
 	
 	@GetMapping("/greeting")
 	public String greeting() {
