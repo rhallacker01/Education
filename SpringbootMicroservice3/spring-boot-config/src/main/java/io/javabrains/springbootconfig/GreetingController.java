eecc7d0a8c63cd7fbd3db29181eff47e8f427490
rhallacker01 <48366396+rhallacker01@users.noreply.github.com>
2021-01-06 07:43:44 -0500

    Environment Object
    Using Environment Object
    	For Edu only; don't use this object
    	Use injection, not env lookup

--- SpringbootMicroservice3/spring-boot-config/src/main/java/io/javabrains/springbootconfig/GreetingController.java
+++ SpringbootMicroservice3/spring-boot-config/src/main/java/io/javabrains/springbootconfig/GreetingController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,8 @@ public class GreetingController {
 	@Autowired
 	private DbSettings dbSettings;
 	
+	@Autowired
+	private Environment env;
 	
 	public GreetingController() {
 		
@@ -35,4 +38,12 @@ public class GreetingController {
 	public String greeting() {
 		return greetingMessage + dbSettings.getConnection() + dbSettings.getHost();
 	}	
+
+	@GetMapping("/envdetails")
+	public String evnDetails() {
+		return env.toString();
+	}
+
+
+
 }
